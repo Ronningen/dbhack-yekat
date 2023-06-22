2c25a558c01803071cfb329d9345858eacaf04e6
Ronningen <step-lap@mail.ru>
2023-06-22 19:18:10 +0300

    desktop app

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 """
     Scrypt to control all models and provide results to the app
 """
-
+import lap
 import pathlib
 import os
 
@@ -11,11 +11,22 @@ from ultralytics.yolo.engine.results import Results
 
 from detector import Detector
 from classifier import Classifier
-from metamodel import Meta
+from metamodel import Meta, std_predict
 
 
 ROOT = pathlib.Path(__file__).parent
 BUFF = 10
+
+
+def _buff2track(buff) -> dict:
+    tracks = {}
+    for sample in buff:
+        for obj in sample.tolist():
+            # xyxy conf
+            id = obj[4]
+            if id:
+                tracks[id] = tracks.get(id,[]) + [obj[:4]+[obj[5]]]
+    return tracks
 
 
 class Controller():
@@ -26,11 +37,39 @@ class Controller():
         if torch.cuda.is_available():
             device='0'
 
-        self.detector = Detector(ROOT.joinpath('bin/detector.onnx'), device)
-        self.classifier = Classifier(ROOT.joinpath('bin/classifier.onnx'), device)
-        self.meta = Meta(ROOT.joinpath('bin/meta.cbm'))
+        self.detector = Detector(ROOT.joinpath('bin/v8m_ep3.pt'), device)
+        # self.classifier = Classifier(ROOT.joinpath('bin/classifier.onnx'), device)
+        # self.meta = Meta(ROOT.joinpath('bin/meta.cbm'))
+    
+    def predict(self, source, show=False):
+        """
+            Predict from single source
+        """
+        results = []
+        buffer = []
 
-    def predict(self, source):
+        for result in self.detector.predict(source, self.stream, show=show):
+            # cls = self.classifier.predict(result) # TODO
+
+            buffer.append(result.boxes.data)
+            if len(buffer) > BUFF:
+                buffer.pop(0)
+
+            tracks = _buff2track(buffer)
+            activities = {}
+            for id in tracks:
+                # action.append(self.meta.predict(tracks[id])) 
+                # activities.append(std_predict(tracks[id]))
+                activities[id] = std_predict(tracks[id])
+            if self.stream:
+                yield activities
+            else:
+                results.append(activities) 
+
+        if not self.stream:
+            return results
+    
+    def predict_all(self, source):
         """
             Predicts from source or from each file in a source if directory
         """
@@ -42,26 +81,12 @@ class Controller():
         if isdir:
             for leaf in os.walk(source):
                 if len(leaf[2])>0: # TODO: check if contatins images/videos
-                    rows.extend(self._predict(leaf[0]))
+                    rows.extend(self.predict(leaf[0]))
         else: 
-            rows.extend(self._predict(source))
-        pd = pd.DataFrame(data=rows,columns=['time','movement'])
-        return pd
-    
-    def _predict(self, source):
-        """
-            Predict from single source
-        """
-        action = []
-        buffer = []
+            rows.extend(self.predict(source))
+        return rows
 
-        for result in self.detector.predict(source, self.stream):
-            cls = self.classifier.predict(result) # TODO
 
-            buffer.append( (result.boxes.data, cls) )
-            if buffer.size > BUFF:
-                buffer.pop(0)
-
-            action.append(self.meta.predict(buffer)) # TODO
-        
-        return action
+if __name__=='__main__':
+    c = Controller(True)
+    c.predict('/Users/samedi/Downloads/613iFGJm0HM_1906_04.mp4')
--- metamodel.py
+++ metamodel.py
@@ -3,6 +3,8 @@
 """
 
 from catboost import CatBoostRegressor as CatBoost
+import numpy as np
+
 
 class Meta():
     def __init__(self, path) -> None:
@@ -11,3 +13,11 @@ class Meta():
 
     def predict(self, X):
         return self.model.predict(X)
+    
+
+def std_predict(X):
+    return np.tanh((np.std(np.array(X), axis=0)/3).sum())
+
+
+if __name__=='__main__':
+    print(std_predict([[0,0,0],[0,1,2],[0,-1,-2]]))
