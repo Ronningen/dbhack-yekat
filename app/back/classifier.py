8b8c2297a3889184efa856e633cb0b49c231adcf
User <roma.leontev.03@bk.ru>
2023-06-24 13:54:21 +0300

    classifier_inference_first

--- app/back/classifier.py
+++ app/back/classifier.py
@@ -1,13 +1,45 @@
 """
     Scrypt for classifier model inference
 """
-
 import torch
+import torch
+from torchvision.models.video import mvit_v2_s, MViT_V2_S_Weights
+
+model.head[1] = torch.nn.Linear(768, 2)
+
 
 class Classifier():
     def __init__(self, path, device='cpu') -> None:
-        self.model = None
-        self.device = device
+        '''
 
-    def predict(self, image):
-        return 'vehicle'
+        :param path: путь до сохраненной модели
+        :param clip_duration:
+        :param device:
+        '''
+        self.model = mvit_v2_s()
+        self.model.eval()
+        self.device = device
+        self.transforms = MViT_V2_S_Weights.KINETICS400_V1.transforms()
+        self.checkpoint = torch.load(path, map_location=self.device)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.idx2class = {0: 'off', 1: 'on'}
+        self.class2idx = {'off': 0, 'on': 1}
+
+    def predict(self, video: torch.FloatTensor):
+        '''
+
+        :param video_path:
+        :return: (pred idx, pred class)
+        '''
+        # full_video_tensor = handle_video_path(video_path)
+        video = video.to(self.device)
+        video = self.transforms(video)
+        pred = torch.argmax(self.model(video.unsqueeze(0)))
+        return pred, idx2class[pred]
+
+    # def handle_video_path(self, video_path: str) -> torch.FloatTensor:
+        # video = EncodedVideo.from_path(video_path)
+        # video_tensor = video.get_clip(0.0, 0.0 + self.clip_duration)['video'].permute(1, 0, 2, 3)
+        # video_tensor = self.transforms(video_tensor)
+        # return video_tensor
+
