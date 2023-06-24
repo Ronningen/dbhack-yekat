5df7b516ea9021c537a33fb0dee0e54b6e1f4c7e
User <roma.leontev.03@bk.ru>
2023-06-24 13:56:39 +0300

    classifier_inference_first

--- app/back/classifier.py
+++ app/back/classifier.py
@@ -27,7 +27,7 @@ class Classifier():
     def predict(self, video: torch.FloatTensor):
         '''
 
-        :param video_path:
+        :param video:
         :return: (pred idx, pred class)
         '''
         # full_video_tensor = handle_video_path(video_path)
